e1111f73b9fc5ff76748ce4fbf7950e8bcbe29b3
Luciano Júnior <luciano.menezesjunior@aluno.senai.br>
2025-06-19 18:26:51 -0300

    Página de login
    Importação de componentes do shadcn ui e implementação de página de login.

---
+++ Frontend/controle_de_acesso/src/app/components/login-form.tsx
@@ -0,0 +1,70 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { ModeToggle } from "./modeToggle"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <ModeToggle/>
+      <Card>
+        <CardHeader>
+          <CardTitle>Entre na sua conta</CardTitle>
+          <CardDescription>
+            Digite seu email abaixo para realizar o login
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Senha</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Esqueceu a senha?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+                
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Não tem conta? {" "}
+              <a href="#" className="underline underline-offset-4">
+                Faça o cadastro
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
