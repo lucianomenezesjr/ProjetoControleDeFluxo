3003af7a00f91edb132fbc33139a6e82721da69a
Luciano Júnior <luciano.menezesjunior@aluno.senai.br>
2025-06-29 00:04:37 -0300

    Config files

--- ControleAcessoAPI/obj/Debug/net9.0/ControleAcessoAPI.AssemblyInfo.cs
+++ ControleAcessoAPI/obj/Debug/net9.0/ControleAcessoAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleAcessoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1ea96ba62904bd0e143093defacd7e6402f714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70136551968aa003d240328c9ec8416bd7b33640")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleAcessoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleAcessoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
